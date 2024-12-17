821f769497d39646b79af44fc2ad61a40a5d1a70
Jan Henning <jh+vsc-hg@buttercookie.de>
2024-12-17 21:05:11 +0100

    Release version 0.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = 23
         targetSdk = 34
         versionCode = 4
-        versionName = "0.3.1a1"
+        versionName = "0.3.1"
         base.archivesName = "$applicationId-$versionName"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
