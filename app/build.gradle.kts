1f1843596288508441a8ca2521725ea91b4e8500
Jan Henning <jh+vsc-hg@buttercookie.de>
2024-12-21 22:54:12 +0100

    Next development version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "de.buttercookie.simbadroid"
         minSdk = 23
         targetSdk = 34
-        versionCode = 5
-        versionName = "0.4"
+        versionCode = 6
+        versionName = "0.5a1"
         base.archivesName = "$applicationId-$versionName"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
